330fdbc911fd981d4788c4fd285a3ff0253c39df
zcantrail <32787710+zcantrail@users.noreply.github.com>
2018-10-22 17:39:02 -0500

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Employee
+    {
+        private string employeeID;
+        private string firstName;
+        private string lastName;
+        private double payRate;
+        private string compIns; //company insurence.
+
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/EmployeeApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class EmployeeApp
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
